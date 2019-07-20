6e80a75c649853bb677b4ab8bb21ce77ecc8a691
boyankov <boyan.yankov@yahoo.com>
2019-07-20 23:51:54 +0300

    added Initial screen

---
+++ MvvmSampleSwift/MvvmSampleSwift/Sources/UILayer/Screens/Initial/View/InitialViewController.swift
@@ -0,0 +1,51 @@
+//
+//  InitialViewController.swift
+//  MvvmSampleSwift
+//
+//  Created by Boyan Yankov on 2019-W29-20-Jul-Sat.
+//  Copyright © 2019 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+import SimpleLogger
+
+class InitialViewController: BaseViewController, InitialViewModelConsumer {
+    
+    // MARK: - Properties
+    private let viewModel: InitialViewModel
+    
+    // MARK: - Initialization
+    @available(*, unavailable, message: "Creating this view controller with `init(coder:)` is unsupported in favor of initializer dependency injection.")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Creating this view controller with `init(coder:)` is unsupported in favor of dependency injection initializer.")
+    }
+    
+    @available(*, unavailable, message: "Creating this view controller with `init(nibName:bundle:)` is unsupported in favor of initializer dependency injection.")
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        fatalError("Creating this view controller with `init(nibName:bundle:)` is unsupported in favor of dependency injection initializer.")
+    }
+    
+    deinit {
+        Logger.fatal.message()
+    }
+    
+    // MARK: - InitialViewModelConsumer protocol
+    required init(viewModel: InitialViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: String(describing: InitialViewController.self), bundle: nil)
+        self.viewModel.setViewModelConsumer(self)
+        Logger.success.message()
+    }
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func showFooScreenButton_touchUpInside(_ sender: Any) {
+        Logger.debug.message()
+    }
+}
